eb45d494582c630b10d43fdc28bc268709e4676b
PLANCON Sylvain <s.plancon@c2is.fr>
2013-06-06 15:38:20 +0200

    Validate composer.json before analyse

--- check-update.sh
+++ check-update.sh
@@ -3,8 +3,22 @@
 if [ "$1" != "" ]; then
     PROJECT_DIR=$1
 else
-    echo "Veuillez indiquer le répertoire de votre fichier composer.json:"
+    printf "Veuillez indiquer le répertoire de votre fichier composer.json:"
     read PROJECT_DIR
 fi
 
-php check-updates.php $PROJECT_DIR
+COMPOSER_FILE=$PROJECT_DIR'/composer.json'
+
+if [ -f $COMPOSER_FILE ]; then
+    VALIDATE=`composer validate $COMPOSER_FILE | grep ' is valid'`
+
+    if [ "$VALIDATE" ]; then
+        php check-updates.php $PROJECT_DIR
+    else
+        printf "Votre fichier fichier composer.json comporte des erreurs. Veuillez les corriger avant vérifier les mises à jour possible."
+        printf "Rapport de composer :"
+        composer validate $COMPOSER_FILE
+    fi
+else
+    printf "Le répertoire indiqué ne comporte pas de fichier composer.json"
+fi
